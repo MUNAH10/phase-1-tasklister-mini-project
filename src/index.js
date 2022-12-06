3596876f9ed60416326ac8c2151539b3f2c2a459
MUNAH10 <munahassan5075@gmail.com>
2022-12-06 15:34:06 +0300

    completed assingment

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
 let form = document.querySelector('form')
 form.addEventListener('submit', e => {
   e.preventDefault()
-  addTask(e.target.addTask.value);
-  form.reset();
+  addTask(e.target.task.value);
+  //form.reset();
 })
 });
 
 function addTask(e){
   var li = document.createElement('li')
-  li.textContent = `${e}`
-  const parent = document. getElementById('tasks')
-  parent.appendChild(li)
-  li.appendChild(btn)
+  li.textContent = e
+  document.querySelector('#list').appendChild(li)
+  //const parent = document. getElementById('tasks')
+  //parent.appendChild(li)
+  //li.appendChild(btn)
 }
